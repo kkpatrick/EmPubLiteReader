73037dd38c7f9436b857aad4d99e3b107f1132ef
Xiaoqian Yu <xiaoqianyupt@gmail.com>
2015-03-19 16:00:19 -0400

    Start the first activity and fragment

--- app/build/generated/source/r/debug/com/commonsware/empublite/R.java
+++ app/build/generated/source/r/debug/com/commonsware/empublite/R.java
@@ -11,14 +11,27 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_about=0x7f020000;
+        public static final int ic_action_help=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+    }
+    public static final class id {
+        public static final int about=0x7f070002;
+        public static final int help=0x7f070001;
+        public static final int progressBar1=0x7f070000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
+    public static final class menu {
+        public static final int options=0x7f060000;
+    }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
+        public static final int about=0x7f040000;
+        public static final int app_name=0x7f040001;
+        public static final int hello_blank_fragment=0x7f040002;
+        public static final int help=0x7f040003;
+        public static final int title_activity_simple_content=0x7f040004;
     }
     public static final class style {
         /**  Customize your theme here. 
--- app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
+++ app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
@@ -1,12 +1,38 @@
 package com.commonsware.empublite;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class EmPubLiteActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.about:
+                Intent intentAbout = new Intent(this, SimpleContentActivity.class);
+                startActivity(intentAbout);
+                return true;
+            case R.id.help:
+                Intent intentHelp = new Intent(this, SimpleContentActivity.class);
+                startActivity(intentHelp);
+                return true;
+            case android.R.id.home:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
