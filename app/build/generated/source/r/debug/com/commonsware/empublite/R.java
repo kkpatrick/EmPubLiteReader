eb01396efdf392ff130df2ea382c0815990f88c5
Xiaoqian Yu <xiaoqianyupt@gmail.com>
2015-03-26 08:56:07 -0400

    redownload the project from git

--- app/build/generated/source/r/debug/com/commonsware/empublite/R.java
+++ app/build/generated/source/r/debug/com/commonsware/empublite/R.java
@@ -16,27 +16,37 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int about=0x7f070003;
-        public static final int help=0x7f070002;
-        public static final int pager=0x7f070001;
-        public static final int progressBar1=0x7f070000;
+        public static final int about=0x7f080003;
+        public static final int help=0x7f080002;
+        public static final int pager=0x7f080001;
+        public static final int progressBar1=0x7f080000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class menu {
-        public static final int options=0x7f060000;
+        public static final int options=0x7f070000;
     }
     public static final class string {
-        public static final int about=0x7f040000;
-        public static final int app_name=0x7f040001;
-        public static final int hello_blank_fragment=0x7f040002;
-        public static final int help=0x7f040003;
-        public static final int title_activity_simple_content=0x7f040004;
+        public static final int about=0x7f050000;
+        public static final int app_name=0x7f050001;
+        public static final int hello_blank_fragment=0x7f050002;
+        public static final int help=0x7f050003;
+        public static final int keepscreenon_summary=0x7f050004;
+        public static final int keepscreenon_title=0x7f050005;
+        public static final int lastpositionsummary=0x7f050006;
+        public static final int lastpositiontitle=0x7f050007;
+        public static final int prefdesc=0x7f050008;
+        public static final int preftitle=0x7f050009;
+        public static final int title_activity_simple_content=0x7f05000a;
     }
     public static final class style {
         /**  Customize your theme here. 
          */
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
+    }
+    public static final class xml {
+        public static final int pref_display=0x7f040000;
+        public static final int preference_headers=0x7f040001;
     }
 }
