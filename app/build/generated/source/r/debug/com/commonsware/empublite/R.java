a1947e84fe526353aaf48fa00b9825c6e8d2f4eb
Xiaoqian Yu <xiaoqianyupt@gmail.com>
2015-03-30 09:25:31 -0400

    add share note feature with ShareActionProvider class. TextWatcher interface used.

--- app/build/generated/source/r/debug/com/commonsware/empublite/R.java
+++ app/build/generated/source/r/debug/com/commonsware/empublite/R.java
@@ -19,14 +19,15 @@ public final class R {
         public static final int ic_launcher=0x7f020005;
     }
     public static final class id {
-        public static final int about=0x7f080005;
-        public static final int delete=0x7f080003;
+        public static final int about=0x7f080006;
+        public static final int delete=0x7f080004;
         public static final int editor=0x7f080000;
-        public static final int help=0x7f080004;
-        public static final int notes=0x7f080007;
+        public static final int help=0x7f080005;
+        public static final int notes=0x7f080008;
         public static final int pager=0x7f080002;
         public static final int progressBar1=0x7f080001;
-        public static final int settings=0x7f080006;
+        public static final int settings=0x7f080007;
+        public static final int share=0x7f080003;
     }
     public static final class layout {
         public static final int editor=0x7f030000;
@@ -51,7 +52,8 @@ public final class R {
         public static final int prefdesc=0x7f05000b;
         public static final int preftitle=0x7f05000c;
         public static final int settings=0x7f05000d;
-        public static final int title_activity_simple_content=0x7f05000e;
+        public static final int share=0x7f05000e;
+        public static final int title_activity_simple_content=0x7f05000f;
     }
     public static final class style {
         /**  Customize your theme here. 
--- app/src/main/java/com/commonsware/empublite/NoteFragment.java
+++ app/src/main/java/com/commonsware/empublite/NoteFragment.java
@@ -1,9 +1,12 @@
 package com.commonsware.empublite;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -11,15 +14,18 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ShareActionProvider;
 
 import de.greenrobot.event.EventBus;
 
 /**
  * Created by abc on 3/27/15.
  */
-public class NoteFragment extends Fragment {
+public class NoteFragment extends Fragment implements TextWatcher{
     private static final String KEY_POSITION = "position";
     private EditText editor = null;
+    private ShareActionProvider share = null;
+    private Intent shareIntent = new Intent(Intent.ACTION_SEND).setType("text/plain");
 
     public interface Contract {
         void closeNotes();
@@ -46,6 +52,7 @@ public class NoteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View result = inflater.inflate(R.layout.editor, container, false);
         editor = (EditText)result.findViewById(R.id.editor);
+        editor.addTextChangedListener(this);
         return result;
     }
 
@@ -69,6 +76,8 @@ public class NoteFragment extends Fragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.notes, menu);
+        share = (ShareActionProvider)menu.findItem(R.id.share).getActionProvider();
+        share.setShareIntent(shareIntent);
         super.onCreateOptionsMenu(menu, inflater);
     }
 
@@ -92,7 +101,23 @@ public class NoteFragment extends Fragment {
         }
     }
 
+    //contract pattern, 需要关闭fragment所在的activity，用到contract pattern。
     private Contract getContract() {
         return (Contract)getActivity();
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        shareIntent.putExtra(Intent.EXTRA_TEXT, editable.toString());
     }
 }
